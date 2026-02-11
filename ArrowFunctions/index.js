a802734ad72a2fe84205e020bc9dfc4a1a284171
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-02-11 21:38:42 +0530

    practice arrow function

--- ArrowFunctions/index.js
+++ ArrowFunctions/index.js
@@ -7,3 +7,25 @@
 // const add = (a, b) => {
 //   return a + b;
 // };
+
+
+// const square = num => num * num;
+// console.log(square(4)); // 16
+
+
+// const numbers = [1, 2, 3, 4];
+// const doubled = numbers.map(n => n * 2);
+// console.log(doubled); // [2, 4, 6, 8]
+
+
+// const checkEven = num => {
+//   if (num % 2 === 0) {
+//     return "Even";
+//   } else {
+//     return "Odd";
+//   }
+// };
+
+// console.log(checkEven(7)); // Odd
+// console.log(checkEven(10)); // Even
+

5fc49dcfc8dc972329b2d0475fb007799f6f5b3f
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-02-12 21:21:04 +0530

    started learning callback functions

---
+++ CallbackFunctions/index.js
@@ -0,0 +1,14 @@
+// 📞 Callback Function in JavaScript 
+// A callback function in JavaScript is a function that is passed as an argument to another function and is executed later, usually after some operation has completed.
+
+
+// In simple terms:
+// A callback is a function you give to another function so it can “call you back” later.
+
+
+// 🔹 Why Callbacks Are Important
+// JavaScript is single-threaded and asynchronous. This means:
+// It runs one task at a time.
+// Some operations (like fetching data, reading files, timers) take time.
+// Instead of blocking the program, JavaScript uses callbacks to handle results when they’re ready.
+

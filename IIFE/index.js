e078c2cd03654a89b33ed99c94be9ed88878f9d5
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-03-04 22:18:18 +0530

    learn about types of iife

--- IIFE/index.js
+++ IIFE/index.js
@@ -12,3 +12,54 @@
 (() => {
   console.log("This also runs immediately!");
 })();
+
+
+
+// ✅ Why Use an IIFE?
+// 1️⃣ Avoid Polluting the Global Scope
+
+// Variables inside an IIFE are private and won’t affect global variables.
+
+// (function () {
+//   let message = "Hello";
+//   console.log(message);
+// })();
+
+// console.log(message); // ❌ Error: message is not defined
+// 2️⃣ Create a Private Scope
+
+// Before let and const existed, IIFEs were commonly used to create scope.
+
+// var counter = (function () {
+//   let count = 0;
+
+//   return function () {
+//     count++;
+//     return count;
+//   };
+// })();
+
+// console.log(counter()); // 1
+// console.log(counter()); // 2
+
+// Here:
+
+// The outer function runs immediately.
+
+// It returns an inner function.
+
+// count remains private (closure).
+
+
+
+// 🔹 Named IIFE
+// (function greet() {
+//   console.log("Hello!");
+// })();
+
+// The name greet is only available inside the function.
+
+// 🔹 IIFE with Parameters
+// (function (name) {
+//   console.log("Hello " + name);
+// })("John");

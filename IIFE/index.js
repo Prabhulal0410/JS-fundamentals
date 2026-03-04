f2b0962e6432929ca7b496c778aafefffc29db47
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-03-04 22:16:17 +0530

    started learning about iife

---
+++ IIFE/index.js
@@ -0,0 +1,14 @@
+// IIFE in JavaScript
+
+// IIFE stands for Immediately Invoked Function Expression.
+
+// It’s a function that runs as soon as it is defined.
+
+// 🔹 Basic Syntax
+(function () {
+  console.log("This runs immediately!");
+})();
+// 🔹 Arrow Function Version
+(() => {
+  console.log("This also runs immediately!");
+})();

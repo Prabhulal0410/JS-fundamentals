d2bc21f19cd9f65d2bb43ea9e5d87199021c8fc3
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-03-07 21:44:33 +0530

    started learning about promises

---
+++ Promise/index.js
@@ -0,0 +1,7 @@
+//  Promise is an object that represents the eventual result of an asynchronous operation. It lets you handle success or failure without blocking the rest of the program.
+
+// Think of a promise like ordering food at a restaurant 🍔:
+// You place an order → Promise is created
+// Food is being prepared → Pending
+// You get the food → Fulfilled (resolved)
+// Restaurant runs out of ingredients → Rejected

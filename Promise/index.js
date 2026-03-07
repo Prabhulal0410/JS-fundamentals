1f5d684d5a6c9b416cf589971a5d7858c56d9f65
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-03-07 21:50:24 +0530

    started learning about promises states

--- Promise/index.js
+++ Promise/index.js
@@ -1,7 +1,11 @@
 //  Promise is an object that represents the eventual result of an asynchronous operation. It lets you handle success or failure without blocking the rest of the program.
 
-// Think of a promise like ordering food at a restaurant 🍔:
-// You place an order → Promise is created
-// Food is being prepared → Pending
-// You get the food → Fulfilled (resolved)
-// Restaurant runs out of ingredients → Rejected
+
+
+// A Promise has 3 states:
+
+// Pending – operation still running
+// Fulfilled (Resolved) – operation completed successfully
+// Rejected – operation failed
+
+// Once a promise is fulfilled or rejected, it cannot change again.

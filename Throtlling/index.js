1c204cd11863651b8f6b415cbe7e2aed9efa71e5
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-06-23 21:09:11 +0530

    practice thortlling

--- Throtlling/index.js
+++ Throtlling/index.js
@@ -25,3 +25,36 @@ window.addEventListener("scroll", throttledScroll);
 
 // Output:
 // Even if you scroll continuously, "Scrolling..." will print at most once every 1 second.
+
+
+
+
+// practice
+function throttle(func, delay) {
+  let lastCall = 0;
+
+  return function (...args) {
+    const now = Date.now();
+
+    if (now - lastCall >= delay) {
+      lastCall = now;
+      func.apply(this, args);
+    }
+  };
+}
+
+function submitOrder() {
+  console.log("Order Submitted!");
+}
+
+const throttledSubmit = throttle(submitOrder, 3000);
+
+document
+  .getElementById("submitBtn")
+  .addEventListener("click", throttledSubmit);
+
+
+
+
+
+

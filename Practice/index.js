9808857c297526c3168660f2974b8981cd52b537
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-08-04 23:18:34 +0530

    practice remove dupliacte problem

--- Practice/index.js
+++ Practice/index.js
@@ -1,10 +1,23 @@
 // reverse String
 
-function reverseString(str) {
-    let reversed = "";
-    for(let i = str.length-1;i>=0;i--){
-        reversed = reversed + str[i]
+// function reverseString(str) {
+//     let reversed = "";
+//     for(let i = str.length-1;i>=0;i--){
+//         reversed = reversed + str[i]
+//     }
+//     return reversed
+// }
+// console.log(reverseString("hello"))
+
+
+// remove duplicate
+function removeDplicte(arr){
+ let duplicte = []
+ for(let i = 0; i< arr.length;i++){
+    if(!duplicte.includes(arr[i])){
+        duplicte.push(arr[i])
     }
-    return reversed
+ }
+ return duplicte
 }
-console.log(reverseString("hello"))
+console.log(removeDplicte([1,2,3,2,4,5,1]))

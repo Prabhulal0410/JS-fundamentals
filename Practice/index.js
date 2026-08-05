0387105bd3969b36ef0c8b938ded8b4ef479c364
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-08-05 21:50:32 +0530

    practice palindrom quetion

--- Practice/index.js
+++ Practice/index.js
@@ -11,13 +11,32 @@
 
 
 // remove duplicate
-function removeDplicte(arr){
- let duplicte = []
- for(let i = 0; i< arr.length;i++){
-    if(!duplicte.includes(arr[i])){
-        duplicte.push(arr[i])
+// function removeDplicte(arr){
+//  let duplicte = []
+//  for(let i = 0; i< arr.length;i++){
+//     if(!duplicte.includes(arr[i])){
+//         duplicte.push(arr[i])
+//     }
+//  }
+//  return duplicte
+// }
+// console.log(removeDplicte([1,2,3,2,4,5,1]))
+
+
+// Check Palindrome
+function isPalindrome(str) {
+    let left = 0;
+    let right = str.length - 1;
+
+    while (left < right) {
+        if (str[left] !== str[right]) {
+            return false;
+        }
+        left++;
+        right--;
     }
- }
- return duplicte
+
+    return true;
 }
-console.log(removeDplicte([1,2,3,2,4,5,1]))
+
+console.log(isPalindrome("madam"));

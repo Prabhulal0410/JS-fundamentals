a03093717a7732aafb1f049c8926e01add613f78
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-08-07 21:54:12 +0530

    practice count vowel problem

--- Practice/index.js
+++ Practice/index.js
@@ -91,3 +91,18 @@
 //     }
 // }
 // console.log(unique);
+
+
+
+// Count Vowels
+// function countVowels(str){
+//     let count = 0;
+//     let vowels = "aeiouAEIOU";
+//     for(let char of str){
+//         if(vowels.includes(char)){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// console.log(countVowels("JavaScript"));

f515a05706c9c89f6df218c14a75722b1c0343af
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-08-08 21:50:37 +0530

    practice factorial problem

--- Practice/index.js
+++ Practice/index.js
@@ -106,3 +106,14 @@
 //     return count;
 // }
 // console.log(countVowels("JavaScript"));
+
+
+// Factorial
+// function factorial(n){
+//     let result = 1;
+//     for(let i=1;i<=n;i++){
+//         result *= i;
+//     }
+//     return result;
+// }
+// console.log(factorial(5));

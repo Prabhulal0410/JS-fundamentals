75140434e4fe3829e4ab8ab6fd4e9bc0337db922
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-08-09 23:02:35 +0530

    practice fibonacci problem

--- Practice/index.js
+++ Practice/index.js
@@ -117,3 +117,18 @@
 //     return result;
 // }
 // console.log(factorial(5));
+
+
+// Fibonacci Series
+// let n = 10;
+// let a = 0;
+// let b = 1;
+// console.log(a);
+// console.log(b);
+
+// for(let i=2;i<n;i++){
+//     let c = a + b;
+//     console.log(c);
+//     a = b;
+//     b = c;
+// }

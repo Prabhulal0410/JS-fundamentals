8c7acaafb22d09a10ed869bfd74030a50a55702f
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-08-18 21:35:29 +0530

    practice bubblesort problem

--- Practice/index.js
+++ Practice/index.js
@@ -236,3 +236,18 @@
 //     right--;
 // }
 // console.log(arr);
+
+
+// Sort Array (Bubble Sort)
+// let arr = [5,3,8,1,2];
+
+// for(let i=0;i<arr.length;i++){
+//     for(let j=0;j<arr.length-i-1;j++){
+//         if(arr[j] > arr[j+1]){
+//             let temp = arr[j];
+//             arr[j] = arr[j+1];
+//             arr[j+1] = temp;
+//         }
+//     }
+// }
+// console.log(arr);

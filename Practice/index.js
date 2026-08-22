fd9e17fb9cdd824f4274e56dd522d19a12109088
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-08-22 21:35:58 +0530

    practice check anagram

--- Practice/index.js
+++ Practice/index.js
@@ -292,3 +292,15 @@
 //     }
 // }
 // console.log(duplicates);
+
+
+// Check Anagram
+// function isAnagram(str1, str2) {
+//     if (str1.length !== str2.length) {
+//         return false;
+//     }
+//     let a = str1.split("").sort().join("");
+//     let b = str2.split("").sort().join("");
+//     return a === b;
+// }
+// console.log(isAnagram("listen", "silent"));

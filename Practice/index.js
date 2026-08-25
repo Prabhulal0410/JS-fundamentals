6ed53d5042033496e6b0a356f3ec67e4ca701669
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-08-25 21:40:44 +0530

    practice intersection without problem

--- Practice/index.js
+++ Practice/index.js
@@ -334,3 +334,18 @@
 //     result.push(0);
 // }
 // console.log(result);
+
+
+
+// Find Intersection Without includes()
+// let arr1 = [1, 2, 3, 4];
+// let arr2 = [2, 4, 6, 8];
+// let result = [];
+// for (let i = 0; i < arr1.length; i++) {
+//     for (let j = 0; j < arr2.length; j++) {
+//         if (arr1[i] === arr2[j]) {
+//             result.push(arr1[i]);
+//         }
+//     }
+// }
+// console.log(result);

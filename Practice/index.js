b12cc0c587db276441a6f19ce85165bef8d78b6c
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-08-30 20:58:39 +0530

    practice binary search problem

--- Practice/index.js
+++ Practice/index.js
@@ -391,3 +391,25 @@
 //     return sum;
 // }
 // console.log(sumOfDigits(12345));
+
+
+
+// Binary Search ⭐
+// function binarySearch(arr, target) {
+//     let left = 0;
+//     let right = arr.length - 1;
+//     while (left <= right) {
+//         let mid = Math.floor((left + right) / 2);
+//         if (arr[mid] === target) {
+//             return mid;
+//         }
+//         if (arr[mid] < target) {
+//             left = mid + 1;
+//         } else {
+//             right = mid - 1;
+//         }
+//     }
+//     return -1;
+// }
+// let arr = [10, 20, 30, 40, 50];
+// console.log(binarySearch(arr, 40));

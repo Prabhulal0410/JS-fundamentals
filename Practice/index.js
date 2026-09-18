610daaa1d9e930a408744c6d35be63b0e07c7b75
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-09-18 10:41:39 +0530

    practice find sum problem

--- Practice/index.js
+++ Practice/index.js
@@ -610,3 +610,18 @@
 //     }
 // }
 // console.log(count);
+
+
+// Find the Sum of Even Numbers
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// let sum = 0;
+
+// for (let num of arr) {
+
+//     if (num % 2 === 0) {
+//         sum += num;
+//     }
+// }
+
+// console.log(sum);

c77b42132ae1105013448957119eefca79b8d0d1
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-09-19 17:09:06 +0530

    pratice revese number problem

--- Practice/index.js
+++ Practice/index.js
@@ -625,3 +625,14 @@
 // }
 
 // console.log(sum);
+
+
+// Reverse a Number
+// let num = 12345;
+// let reverse = 0;
+// while (num > 0) {
+//     let digit = num % 10;
+//     reverse = reverse * 10 + digit;
+//     num = Math.floor(num / 10);
+// }
+// console.log(reverse);

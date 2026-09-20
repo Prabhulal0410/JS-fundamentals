0e4cd3afe58af22d58102a0b9d77d3e1bd07ffef
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-09-20 21:22:20 +0530

    practice check is numbr palindrom problem

--- Practice/index.js
+++ Practice/index.js
@@ -636,3 +636,18 @@
 //     num = Math.floor(num / 10);
 // }
 // console.log(reverse);
+
+
+// Check Whether a Number is Palindrome
+// function isPalindrome(num) {
+//     let original = num;
+//     let reverse = 0;
+//     while (num > 0) {
+//         let digit = num % 10;
+//         reverse = reverse * 10 + digit;
+//         num = Math.floor(num / 10);
+//     }
+//     return original === reverse;
+// }
+// console.log(isPalindrome(121));
+// console.log(isPalindrome(123));
